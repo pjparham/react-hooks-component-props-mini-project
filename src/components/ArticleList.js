af67b16612c9182c93780f90b8743ea446d3acff
Parker Parham <parkerparham@gmail.com>
2022-09-07 12:02:49 -0400

    commit tree a7f21000c0c8b5cc5cebb8d61b32eb578ef41fb2

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Article from "./Article";
 
-function ArticleList({blogPosts}){
+function ArticleList({posts}){
     
-    const displayArticles = blogPosts.map((blogPost) => {
+    const displayArticles = posts.map((blogPost) => {
         return (
             <Article title ={blogPost.title} date={blogPost.date} preview={blogPost.preview} key={blogPost.id} minutes={blogPost.minutes}/>
         )
